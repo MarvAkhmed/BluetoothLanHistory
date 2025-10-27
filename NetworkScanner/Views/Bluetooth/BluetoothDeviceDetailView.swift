2636929a9e9411c01c4927ebc5c284ee1310aabd
Marwa <MarvAkhmed@stud.kpfu.ru>
2025-10-28 02:36:51 +0300

    detailed screen for both lan and bluetooth devices

---
+++ NetworkScanner/Views/Bluetooth/BluetoothDeviceDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothDeviceDetailView.swift
+//  NetworkScannerP
+//
+//  Created by Marwa Awad on 28.10.2025.
+//
+
+import Foundation
---
+++ NetworkScanner/Views/LAN/LanDeviceDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  LanDeviceDetailView.swift
+//  NetworkScannerP
+//
+//  Created by Marwa Awad on 28.10.2025.
+//
+
+import Foundation
